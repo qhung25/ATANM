d2be0f89ad1999a549fe0cad10def7043e88e89f
qhung25 <157773878+qhung25@users.noreply.github.com>
2026-03-17 14:38:08 +0700

    Add files via upload

--- nhom_13.sql
+++ nhom_13.sql
@@ -1,35 +1,11 @@
-﻿-- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 17, 2026 at 07:26 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
-CREATE DATABASE nhom_13;
+﻿-- XÓA DATABASE CŨ (nếu có)
+DROP DATABASE IF EXISTS nhom_13;
+
+-- TẠO DATABASE
+CREATE DATABASE nhom_13 CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE nhom_13;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `nhom_13`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
--- Tạo bảng thành viên
+-- Tạo bảng users
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     fullname VARCHAR(100) NOT NULL,
@@ -39,8 +15,8 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Chèn dữ liệu 3 thành viên từ ảnh
+-- Thêm dữ liệu mẫu
 INSERT INTO users (fullname, role, username, password) VALUES 
 ('Trương Vang Việt', 'Kỹ Thuật', 'vangviet', '123'),
-('Quốc Hùng', 'Trưởng Nhóm', 'quochung', '456'),
+('Đặng Quốc Hùng', 'Trưởng Nhóm', 'quochung', '456'),
 ('Bảo Nhi', 'Thiết Kế', 'baonhi', '789');
